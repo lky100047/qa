3f0ee5252413dbcdc10143b5a2a5a022f536f5d1
lky100047 <47895484+lky100047@users.noreply.github.com>
2022-01-10 12:23:46 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+model_name = "deepset/roberta-base-squad2"
+
+
+@st.cache(allow_output_mutation=True)
+def load_qa_model():
+    model = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    return model
+
+qa = load_qa_model()
+st.title("Ask Questions about your Text")
+sentence = st.text_area('Please paste your article :', height=30)
+question = st.text_input("Questions from this article?")
+button = st.button("Get me Answers")
+max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
+min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
+do_sample = st.sidebar.checkbox("Do sample", value=False)
+with st.spinner("Discovering Answers.."):
+    if button and sentence:
+        answers = qa(question=question, context=sentence)
+        st.write(answers['answer'])
